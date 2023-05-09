6086eb4c515422a19ae1e0ad0d96efb8e2d69065
Олег Бовкуш <obovkush@gmail.com>
2023-05-09 13:46:36 +1000

    Добавлена маршрутизация с ленивой загрузкой

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { Counter } from "./components/Counter";
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
+import { Link } from 'react-router-dom';
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
+import { Suspense } from 'react';
+import Loading from './components/Loading';
 
 const App: React.FC = () => (
-	<div>
-		<Counter />
+	<div className='app'>
+		<Link to={'/'}>MainPage</Link>
+		<Link to={'/about'}>AboutPage</Link>
+		<Suspense fallback={<Loading/>}>
+			<Routes>
+				<Route path={'/about'} element={<AboutPageLazy />} />
+				<Route path={'/'} element={<MainPageLazy />} />
+			</Routes>
+		</Suspense>
 	</div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<App />
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
