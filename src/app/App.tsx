a68de4daa475ca059c88b59fb20d9fb10a6c0fa8
Олег Бовкуш <obovkush@gmail.com>
2023-05-10 00:44:32 +1000

    Подготовлена FSD архитектура

---
+++ src/app/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { Suspense } from 'react';
+import './styles/index.scss';
+import { classNames } from 'shared/lib/helpers/classes/classNames';
+import { useTheme } from './providers/ThemeProvider';
+
+
+const App: React.FC = () => {
+	const { theme, toggleTheme } = useTheme();
+	return (
+		<div className={classNames('app', {}, [theme])}>
+			<Link to={'/'}>MainPage</Link>
+			<Link to={'/about'}>AboutPage</Link>
+			<button onClick={toggleTheme}>Тема</button>
+			<Suspense fallback={<p><i>Loading...</i></p>}>
+				<Routes>
+					<Route path={'/about'} element={<AboutPage />} />
+					<Route path={'/'} element={<MainPage />} />
+				</Routes>
+			</Suspense>
+		</div>
+	);
+};
+
+export default App;
