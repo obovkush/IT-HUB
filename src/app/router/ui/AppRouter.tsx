9792a0ae059d2c8806d8fee5cff089826907abdf
Олег Бовкуш <obovkush@gmail.com>
2023-05-23 13:19:46 +1000

    Доработан AppRouter

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -4,16 +4,14 @@ import {router} from './router';
 
 const AppRouter = () => {
 	return (
-		<Suspense
-			fallback={
-				<p>
-					<i>Loading...</i>
-				</p>
-			}
-		>
+		<Suspense fallback={<p><i>Loading...</i></p>}>
 			<Routes>
 				{Object.values(router).map(({element, path}) => (
-					<Route path={path} element={element} />
+					<Route key={path} path={path} element={(
+						<Suspense fallback={<div>Loading...</div>}>
+							{element}
+						</Suspense>
+					)} />
 				))}
 			</Routes>
 		</Suspense>
