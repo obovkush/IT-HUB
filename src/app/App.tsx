6bdc6ddd1c1c61cde57066b27cd92fb0f998a2e9
Олег Бовкуш <obovkush@gmail.com>
2023-05-23 13:19:18 +1000

    Скорректирован App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
-import {Link} from 'react-router-dom';
 import './styles/index.scss';
 import {useTheme} from './providers/ThemeProvider';
 import {Navbar} from 'widjets/ui/Navbar';
 import {classNames} from 'shared/lib/classNames/classNames';
+import {AppRouter} from './router';
 
 const App: React.FC = () => {
-	const {theme, toggleTheme} = useTheme();
+	const {theme} = useTheme();
 	return (
 		<div className={classNames('app', {}, [theme])}>
 			<Navbar />
-			<Link to={'/about'}>AboutPage</Link>
-			<button onClick={toggleTheme}>Тема</button>
+			<AppRouter />
 		</div>
 	);
 };
