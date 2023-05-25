7af5b0d7ede5eac8033920a226c8b4ec8fc39468
Олег Бовкуш <obovkush@gmail.com>
2023-05-25 11:23:53 +1000

    Sidebar добавлен в App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,19 @@
 import './styles/index.scss';
-import {useTheme} from './providers/ThemeProvider';
-import {Navbar} from 'widjets/Navbar';
-import {classNames} from 'shared/lib/classNames/classNames';
 import {AppRouter} from './router';
+import {useTheme} from './providers/ThemeProvider';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {Navbar} from 'widjets/Navbar';
+import {Sidebar} from 'widjets/Sidebar';
 
 const App: React.FC = () => {
 	const {theme} = useTheme();
 	return (
 		<div className={classNames('app', {}, [theme])}>
 			<Navbar />
-			<AppRouter />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
 		</div>
 	);
 };
