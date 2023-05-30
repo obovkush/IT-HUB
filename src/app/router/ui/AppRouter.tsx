ad3ff94e3deaa76c8081729f1dcc837ba5a6ea4e
Олег Бовкуш <obovkush@gmail.com>
2023-05-31 01:13:35 +1000

    Добавлен загрузчик при переходе на страницы

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -1,6 +1,7 @@
 import {Suspense} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {router} from './router';
+import {PageLoader} from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => {
     return (
@@ -8,7 +9,7 @@ const AppRouter = () => {
             <Routes>
                 {Object.values(router).map(({element, path}) => (
                     <Route key={path} path={path} element={(
-                        <Suspense fallback={<div>Loading...</div>}>
+                        <Suspense fallback={<PageLoader />}>
                             {element}
                         </Suspense>
                     )} />
