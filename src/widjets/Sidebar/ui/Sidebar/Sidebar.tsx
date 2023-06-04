349a8f25a4a3891411cab295278cf0b819e1328a
Олег Бовкуш <obovkush@gmail.com>
2023-06-04 13:11:52 +1000

    Тесты сайдбара и кнопки

--- src/widjets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widjets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,12 +1,12 @@
-import {useState} from "react";
+import {useState} from 'react';
 import {classNames} from 'shared/lib/classNames/classNames';
 import cls from './Sidebar.module.scss';
-import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher';
 import {LangSwitcher} from 'shared/ui/LangSwitcher/LangSwitcher';
-import {useTranslation} from "react-i18next";
+import {useTranslation} from 'react-i18next';
 
 interface SidebarProps {
-    className?: string;
+	className?: string;
 }
 
 export const Sidebar = ({className}: SidebarProps) => {
@@ -14,19 +14,23 @@ export const Sidebar = ({className}: SidebarProps) => {
     const {t} = useTranslation();
 
     const onToggle = () => {
-        setCollapsed(prev => !prev);
-    }
+        setCollapsed((prev) => !prev);
+    };
 
     return (
         <div
-            className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
+            data-testid="sidebar"
+            className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [
+                className,
+            ])}
         >
-            <button onClick={onToggle}>{t('Скрыть')}</button>
+            <button data-testid='sidebar-toggle' onClick={onToggle}>
+                {t('Скрыть')}
+            </button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang}/>
+                <LangSwitcher className={cls.lang} />
             </div>
         </div>
     );
 };
-
