c3e3003e9779a9b212926546edb011c1007ebd7b
Олег Бовкуш <obovkush@gmail.com>
2023-06-05 19:58:41 +1000

    Форматирование еслинта

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true,
+        es2021: true
     },
-    extends: ['plugin:react/recommended', 'plugin:i18next/recommended'],
+    extends: ["plugin:react/recommended", "plugin:i18next/recommended", "plugin:storybook/recommended"],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
-            jsx: true,
+            jsx: true
         },
         ecmaVersion: 'latest',
-        sourceType: 'module',
+        sourceType: 'module'
     },
     plugins: ['react', '@typescript-eslint', 'i18next'],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [
-            2,
-            {extensions: ['.js', '.jsx', '.tsx']},
-        ],
+        'react/jsx-filename-extension': [2, {
+            extensions: ['.js', '.jsx', '.tsx']
+        }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -32,24 +31,22 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': [
-            'error',
-            {
-                markupOnly: true,
-                ignoreAttribute: ['data-testid'],
-            },
-        ],
-        'max-len': ['error', {ignoreComments: true, code: 120}],
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true,
+            ignoreAttribute: ['data-testid', 'to']
+        }],
+        'max-len': ['error', {
+            ignoreComments: true,
+            code: 120
+        }]
     },
     globals: {
-        __IS_DEV__: true,
+        __IS_DEV__: true
     },
-    overrides: [
-        {
-            files: ['**/src/**/*.test.{ts,tsx}'],
-            rules: {
-                'i18next/no-literal-string': 'off',
-            },
-        },
-    ],
+    overrides: [{
+        files: ['**/src/**/*.test.{ts,tsx}'],
+        rules: {
+            'i18next/no-literal-string': 'off'
+        }
+    }]
 };
