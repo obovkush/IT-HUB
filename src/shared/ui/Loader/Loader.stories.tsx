2064a27bd962c1ca57fcef1af9486a7a134ae02f
Олег Бовкуш <obovkush@gmail.com>
2023-06-05 20:05:53 +1000

    Добавлена история Loader

---
+++ src/shared/ui/Loader/Loader.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Loader } from './Loader';
+
+export default {
+    title: 'shared/Loader',
+    component: Loader,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {
+        to: '/',
+    },
+} as Meta<typeof Loader>;
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Template: StoryFn<typeof Loader> = (args) => <Loader {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator];
