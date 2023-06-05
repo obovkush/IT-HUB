d8911f810849ff3ed2293c1cea82490e503b63ad
Олег Бовкуш <obovkush@gmail.com>
2023-06-05 20:58:06 +1000

    Добавлена история для ThemeSwitcher

---
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { ThemeSwitcher } from './ThemeSwitcher';
+
+export default {
+    title: 'shared/ThemeSwitcher',
+    component: ThemeSwitcher,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof ThemeSwitcher>;
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Template: StoryFn<typeof ThemeSwitcher> = (args) => <ThemeSwitcher {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator];
