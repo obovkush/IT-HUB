72c626e41acfd0be7034873ee2b6fa754c174fba
Олег Бовкуш <obovkush@gmail.com>
2023-06-05 21:13:18 +1000

    Добавлен сторис Navbar

---
+++ src/widjets/Navbar/ui/Navbar.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Navbar } from './Navbar';
+
+export default {
+    title: 'widget/Navbar',
+    component: Navbar,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof Navbar>;
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Template: StoryFn<typeof Navbar> = (args) => <Navbar {...args} />;
+
+export const Light = Template.bind({});
+Light.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator];
