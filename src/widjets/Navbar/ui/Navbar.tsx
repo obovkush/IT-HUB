513e2596064451c10489246ca86d5fd14dd73d14
Олег Бовкуш <obovkush@gmail.com>
2023-06-13 23:53:20 +1000

    В навбар добавлена кнопка Войти

--- src/widjets/Navbar/ui/Navbar.tsx
+++ src/widjets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,9 @@
 import cls from './Navbar.module.scss';
-import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink';
 import {classNames} from 'shared/lib/classNames/classNames';
 import {useTranslation} from "react-i18next";
+import {useCallback, useState} from 'react';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 
 interface NavbarProps {
@@ -9,21 +11,26 @@ interface NavbarProps {
 }
 
 export const Navbar = ({className}: NavbarProps) => {
+    const [isAuthModal, setIsAuthModal] = useState(false);
     const {t} = useTranslation();
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink
-                    theme={AppLinkTheme.SECONDARY}
-                    to={'/'}
-                    className={cls.mainLink}
-                >
-                    {t('Main')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.RED} to={'/about'}>
-                    {t('About')}
-                </AppLink>
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line */}
+                {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.')}
+            </Modal>
         </div>
     );
 };
