ccd4406b7d8c7c1c7c637c15a104fda5904e7930
Олег Бовкуш <obovkush@gmail.com>
2023-06-14 19:03:53 +1000

    Добавлен StoreProvider

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import ReactDOM from 'react-dom/client';
 import App from 'app/App';
 import {BrowserRouter} from 'react-router-dom';
 import {ThemeProvider} from 'app/providers/ThemeProvider';
+import {StoreProvider} from 'app/providers/StoreProvider';
 import 'app/styles/index.scss';
 import './shared/config/i18n/i18n';
 import {ErrorBoundary} from 'app/providers/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
 );
