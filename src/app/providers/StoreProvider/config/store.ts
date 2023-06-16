16af20ea89ecab679a407dfd7e08247dec6ca841
Олег Бовкуш <obovkush@gmail.com>
2023-06-16 10:21:13 +1000

    Подготовлен слайс редакс User

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { StateSchema } from './StateSchema';
+import { userReducer } from 'entities/User';
 
 export function createReduxStore(initialState?: StateSchema) {
     return configureStore<StateSchema>({
         reducer: {
             counter: counterReducer,
+            user: userReducer,
         },
         devTools: __IS_DEV__,
         preloadedState: initialState,
