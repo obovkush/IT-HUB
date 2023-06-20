68e985ee69dc657d4d7d153fae5492b391ed9a5e
Олег Бовкуш <obovkush@gmail.com>
2023-06-20 18:33:24 +1000

    Доработана форма авторизации с динамической подгрузкой редьюсера

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,15 +1,15 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { Modal } from 'shared/ui/Modal/Modal';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {Modal} from 'shared/ui/Modal/Modal';
 import cls from './LoginModal.module.scss';
-import { LoginForm } from '../LoginForm/LoginForm';
+import LoginForm from '../LoginForm/LoginForm';
 
 interface LoginModalProps {
-    className?: string;
-    isOpen: boolean;
-    onClose: () => void;
+	className?: string;
+	isOpen: boolean;
+	onClose: () => void;
 }
 
-export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
+export const LoginModal = ({className, isOpen, onClose}: LoginModalProps) => (
     <Modal
         className={classNames(cls.LoginModal, {}, [className])}
         isOpen={isOpen}
