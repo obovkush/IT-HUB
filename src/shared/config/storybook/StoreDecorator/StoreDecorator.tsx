b4b5ea9e408e9314f9483404c1d64806af6f76fe
Олег Бовкуш <obovkush@gmail.com>
2023-06-20 18:50:39 +1000

    Исправлен StoreDecorator

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -6,28 +6,13 @@ import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
     loginForm: loginReducer,
 };
+
+export const StoreDecorator = (
+    state: DeepPartial<StateSchema>,
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
 // eslint-disable-next-line react/display-name
-export const StoreDecorator = (state: DeepPartial<StateSchema>,
-    // eslint-disable-next-line react/display-name
-    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,) => (StoryComponent: StoryFn) => (
-        <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
+) => (StoryComponent: StoryFn) => (
+    <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
         <StoryComponent />
     </StoreProvider>
 );
-
-
-// import { Story } from '@storybook/react';
-// import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-// import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
-
-
-
-
-// export const StoreDecorator = (
-//     state: DeepPartial<StateSchema>,
-//     asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
-// ) => (StoryComponent: Story) => (
-//     <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
-//         <StoryComponent />
-//     </StoreProvider>
-// );
