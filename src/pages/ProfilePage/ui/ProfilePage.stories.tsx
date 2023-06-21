433c7487d06bfc483843d3f54eca8d3a8d7efffd
Олег Бовкуш <obovkush@gmail.com>
2023-06-21 12:30:15 +1000

    Исправлен стори для ProfilePage

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,6 +2,7 @@ import type {Meta, StoryObj} from '@storybook/react';
 import ProfilePage from './ProfilePage';
 import {ThemeDecorator} from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import {Theme} from 'app/providers/ThemeProvider';
+import {StoreDecorator} from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 
 const meta: Meta<typeof ProfilePage> = {
     title: 'pages/ProfilePage',
@@ -15,8 +16,9 @@ type Story = StoryObj<typeof ProfilePage>;
 export const Default: Story = {
     render: () => <ProfilePage />,
 };
+Default.decorators = [StoreDecorator({})];
 
 export const Dark: Story = {
     render: () => <ProfilePage />,
 };
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
