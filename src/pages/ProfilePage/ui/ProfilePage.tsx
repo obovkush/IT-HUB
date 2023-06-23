fff635c740d5eb1f414359a3dc4b25894e320c16
Олег Бовкуш <obovkush@gmail.com>
2023-06-23 15:58:46 +1000

    Добавлена каточка профиля

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -4,7 +4,9 @@ import {
     DynamicModuleLoader,
     ReducersList,
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import {profileReducer} from 'entities/Profile';
+import {ProfileCard, fetchProfileData, profileReducer} from 'entities/Profile';
+import {useEffect} from 'react';
+import {useAppDispatch} from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 const reducers: ReducersList = {
     profile: profileReducer,
@@ -16,11 +18,16 @@ interface ProfilePageProps {
 
 const ProfilePage = ({className}: ProfilePageProps) => {
     const {t} = useTranslation();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('', {}, [className])}>
-                {t('PROFILE PAGE')}
+                <ProfileCard />
             </div>
         </DynamicModuleLoader>
     );
