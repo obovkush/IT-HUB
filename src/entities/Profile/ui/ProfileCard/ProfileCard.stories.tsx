3eec48f63b0535f341aa0ab6f5a6c0fbbfe737cd
Олег Бовкуш <obovkush@gmail.com>
2023-06-26 03:22:20 +1000

    Исправление скринов

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -1,11 +1,9 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import {ThemeDecorator} from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import {Theme} from 'app/providers/ThemeProvider';
-import {StoreDecorator} from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import {Profile, ProfileCard} from 'entities/Profile';
 import {Country} from 'entities/Country';
 import {Currency} from 'entities/Currency';
-import avatar from 'shared/assets/tests/storybook.jpg';
 
 const meta: Meta<typeof ProfileCard> = {
     title: 'entities/ProfileCard',
@@ -24,7 +22,7 @@ const data: Profile = {
     age: 42,
     currency: Currency.RUB,
     username: 'admin',
-    avatar,
+    avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
 };
 
 export const Default: Story = {
