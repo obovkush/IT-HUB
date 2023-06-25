9b24fe388f2e118d079191e7f1c0d65b262c93ef
Олег Бовкуш <obovkush@gmail.com>
2023-06-25 21:37:24 +1000

    Доработан сторис Sidebar

--- src/widjets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widjets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,6 +1,9 @@
 import type {Meta, StoryObj} from '@storybook/react';
 
 import {Sidebar} from './Sidebar';
+import {StoreDecorator} from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import {ThemeDecorator} from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import {Theme} from 'app/providers/ThemeProvider';
 
 const meta: Meta<typeof Sidebar> = {
     title: 'widjet/Sidebar',
@@ -14,3 +17,16 @@ type Story = StoryObj<typeof Sidebar>;
 export const Default: Story = {
     render: () => <Sidebar />,
 };
+Default.decorators = [StoreDecorator({})];
+
+export const Dark: Story = {
+    render: () => <Sidebar />,
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+
+export const AuthSidebar: Story = {
+    render: () => <Sidebar />,
+};
+AuthSidebar.decorators = [StoreDecorator({
+    user: { authData: {} },
+})];
