c0f21c3ec6046b8e82560579ffd82f0bd2857895
Олег Бовкуш <obovkush@gmail.com>
2023-06-27 04:21:44 +1000

    Компонент ArticleDetail добавлен на страницу детализации статьи

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,6 +1,8 @@
-import {classNames} from 'shared/lib/classNames/classNames';
-import {useTranslation} from 'react-i18next';
 import {memo} from 'react';
+import {ArticleDetails} from 'entities/Article';
+import {useParams} from 'react-router-dom';
+import {useTranslation} from 'react-i18next';
+import {classNames} from 'shared/lib/classNames/classNames';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
@@ -10,8 +12,21 @@ interface ArticleDetailsPageProps {
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const {className = ''} = props;
     const {t} = useTranslation();
+    const {id} = useParams<{ id: string }>();
 
-    return <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>{'ARTICLE DETAILS'}</div>;
+    if (!id) {
+        return (
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                {t('Статья не найдена')}
+            </div>
+        );
+    }
+
+    return (
+        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+            <ArticleDetails id={id} />
+        </div>
+    );
 };
 
 export default memo(ArticleDetailsPage);
