df7527b1e9bdf3ddd8545231cf3899cb822b7400
Олег Бовкуш <obovkush@gmail.com>
2023-06-27 04:27:14 +1000

    Исправление форматирования

--- src/shared/ui/Text/Text.stories.tsx
+++ src/shared/ui/Text/Text.stories.tsx
@@ -4,8 +4,8 @@ import {ThemeDecorator} from 'shared/config/storybook/ThemeDecorator/ThemeDecora
 import {Theme} from 'app/providers/ThemeProvider';
 
 const meta: Meta<typeof Text> = {
-	title: 'shared/Text',
-	component: Text,
+    title: 'shared/Text',
+    component: Text,
 };
 
 export default meta;
@@ -13,58 +13,58 @@ export default meta;
 type Story = StoryObj<typeof Text>;
 
 export const Default: Story = {
-	args: {
-		title: 'Title lorem ipsum',
-		text: 'Description Description Description Description',
-	},
+    args: {
+        title: 'Title lorem ipsum',
+        text: 'Description Description Description Description',
+    },
 };
 
 export const Error: Story = {
-	args: {
-		title: 'Title lorem ipsum',
-		text: 'Description Description Description Description',
-		theme: TextTheme.ERROR,
-	},
+    args: {
+        title: 'Title lorem ipsum',
+        text: 'Description Description Description Description',
+        theme: TextTheme.ERROR,
+    },
 };
 
 export const OnlyTitle: Story = {
-	args: {
-		title: 'Title lorem ipsum',
-	},
+    args: {
+        title: 'Title lorem ipsum',
+    },
 };
 
 export const OnlyText: Story = {
-	args: {
-		text: 'Description Description Description Description',
-	},
+    args: {
+        text: 'Description Description Description Description',
+    },
 };
 
 export const PrimaryDark: Story = {
-	args: {
-		title: 'Title lorem ipsum',
-		text: 'Description Description Description Description',
-	},
+    args: {
+        title: 'Title lorem ipsum',
+        text: 'Description Description Description Description',
+    },
 };
 PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
 
 export const OnlyTitleDark: Story = {
-	args: {
-		title: 'Title lorem ipsum',
-	},
+    args: {
+        title: 'Title lorem ipsum',
+    },
 };
 OnlyTitleDark.decorators = [ThemeDecorator(Theme.DARK)];
 
 export const OnlyTextDark: Story = {
-	args: {
-		text: 'Description Description Description Description',
-	},
+    args: {
+        text: 'Description Description Description Description',
+    },
 };
 OnlyTextDark.decorators = [ThemeDecorator(Theme.DARK)];
 
 export const SizeL: Story = {
-	args: {
-		title: 'Title lorem ipsum',
-		text: 'Description Description Description Description',
-		size: TextSize.L,
-	},
+    args: {
+        title: 'Title lorem ipsum',
+        text: 'Description Description Description Description',
+        size: TextSize.L,
+    },
 };
