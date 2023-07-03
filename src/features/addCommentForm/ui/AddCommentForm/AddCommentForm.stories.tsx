248a6f796bb4de7ff22b47f93e5d873e89d03cbf
Олег Бовкуш <obovkush@gmail.com>
2023-07-03 12:02:49 +1000

    Добавление фичи добавления комментария

---
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
@@ -0,0 +1,19 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {StoreDecorator} from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { action } from '@storybook/addon-actions';
+import AddCommentForm from './AddCommentForm';
+
+
+const meta: Meta<typeof AddCommentForm> = {
+    title: 'features/AddCommentForm',
+    component: AddCommentForm,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AddCommentForm>;
+
+export const Default: Story = {
+    render: () => <AddCommentForm onSendComment={action('onSendComment')}/>,
+};
+Default.decorators = [StoreDecorator({})];
