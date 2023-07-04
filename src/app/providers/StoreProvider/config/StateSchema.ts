b06a5235bd3639709b7e7ca3c71d5d8b7cb04712
Олег Бовкуш <obovkush@gmail.com>
2023-07-04 11:31:47 +1000

    Добавлена ArticlesPageSchema, селекторыб сервисы, слайс ArticlesPage

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -14,6 +14,7 @@ import {ProfileSchema} from 'entities/Profile';
 import {ArticleDetailsSchema} from 'entities/Article';
 import {ArticleDetailsCommentsSchema} from 'pages/ArticleDetailsPage/model/types/ArticleDetailsCommentsSchema';
 import {AddCommentFormSchema} from 'features/addCommentForm';
+import {ArticlesPageSchema} from 'pages/ArticlesPage/model/types/articlesPageSchema';
 
 export interface StateSchema {
 	counter: CounterSchema;
@@ -25,6 +26,7 @@ export interface StateSchema {
 	articleDetails?: ArticleDetailsSchema;
 	articleDetailsComments?: ArticleDetailsCommentsSchema;
 	addCommentForm?: AddCommentFormSchema;
+	articlesPage?: ArticlesPageSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
