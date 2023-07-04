fe759ff84ebd774d0deb2b2ea51bb8adc0a41d91
Олег Бовкуш <obovkush@gmail.com>
2023-07-04 11:30:01 +1000

    Исправлены относительные пути

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -9,12 +9,12 @@ import {DynamicModuleLoader, ReducersList} from 'shared/lib/components/DynamicMo
 import {useDispatch, useSelector} from 'react-redux';
 import {useInitialEffect} from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import {fetchCommentsByArticleId}
-    from 'pages/ArticleDetailsPage/model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+    from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
 import cls from './ArticleDetailsPage.module.scss';
 import {articleDetailsCommentsReducer, getArticleComments} from '../../model/slices/articleDetailsCommentsSlice';
 import {getArticleCommentsIsLoading} from '../../model/selectors/comments';
 import {AddCommentForm} from 'features/addCommentForm';
-import {addCommentForArticle} from 'pages/ArticleDetailsPage/model/services/addCommentForArticle/addCommentForArticle';
+import {addCommentForArticle} from '../../model/services/addCommentForArticle/addCommentForArticle';
 import {RoutePath} from 'shared/config/routerConfig/routerConfig';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
