3cfbbc2581b1875b9a03a95b06a23091a068bda9
Олег Бовкуш <obovkush@gmail.com>
2023-07-05 15:08:32 +1000

    Добавлена фича UI

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -15,10 +15,12 @@ import {ArticleDetailsSchema} from 'entities/Article';
 import {ArticleDetailsCommentsSchema} from 'pages/ArticleDetailsPage/model/types/ArticleDetailsCommentsSchema';
 import {AddCommentFormSchema} from 'features/addCommentForm';
 import {ArticlesPageSchema} from 'pages/ArticlesPage/model/types/articlesPageSchema';
+import {UISchema} from 'features/UI';
 
 export interface StateSchema {
 	counter: CounterSchema;
 	user: UserSchema;
+	ui: UISchema;
 
 	// Асинхронные редюсеры
 	loginForm?: LoginSchema;
