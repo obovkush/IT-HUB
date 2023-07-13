e136dad7939fe61369fab4e48a7fcf890479acc9
Олег Бовкуш <obovkush@gmail.com>
2023-07-13 11:56:30 +1000

    Доработан компонент ArticleList под фильтры

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,55 +1,46 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
-import { ArticleListItemSkeleton } from '../../ui/ArticleListItem/ArticleListItemSkeleton';
-import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTranslation} from 'react-i18next';
+import {memo} from 'react';
+import {ArticleListItemSkeleton} from '../../ui/ArticleListItem/ArticleListItemSkeleton';
+import {ArticleListItem} from '../ArticleListItem/ArticleListItem';
 import cls from './ArticleList.module.scss';
-import { Article, ArticleView } from '../../model/types/article';
+import {Article, ArticleView} from '../../model/types/article';
+import {Text, TextSize} from 'shared/ui/Text/Text';
 
 interface ArticleListProps {
-    className?: string;
-    articles: Article[]
-    isLoading?: boolean;
-    view?: ArticleView;
+	className?: string;
+	articles: Article[];
+	isLoading?: boolean;
+	view?: ArticleView;
 }
 
-const getSkeletons = (view: ArticleView) => new Array(view === ArticleView.SMALL ? 9 : 3)
-    .fill(0)
-    .map((item, index) => (
-        <ArticleListItemSkeleton className={cls.card} key={index} view={view} />
-    ));
+const getSkeletons = (view: ArticleView) =>
+    new Array(view === ArticleView.SMALL ? 9 : 3)
+        .fill(0)
+        .map((item, index) => <ArticleListItemSkeleton className={cls.card} key={index} view={view} />);
 
 export const ArticleList = memo((props: ArticleListProps) => {
-    const {
-        className = '',
-        articles,
-        view = ArticleView.SMALL,
-        isLoading,
-    } = props;
-    const { t } = useTranslation();
+    const {className = '', articles, view = ArticleView.SMALL, isLoading} = props;
+    const {t} = useTranslation();
 
     if (isLoading) {
+        return <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>{getSkeletons(view)}</div>;
+    }
+
+    const renderArticle = (article: Article) =>
+        <ArticleListItem article={article} view={view} className={cls.card} key={article.id} />;
+
+    if (!isLoading && !articles.length) {
         return (
             <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-                {getSkeletons(view)}
+                <Text size={TextSize.M} title={t('Статьи не найдены')} />
             </div>
         );
     }
 
-    const renderArticle = (article: Article) => (
-        <ArticleListItem
-            article={article}
-            view={view}
-            className={cls.card}
-            key={article.id}
-        />
-    );
-
     return (
         <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-            {articles.length > 0
-                ? articles.map(renderArticle)
-                : null}
+            {articles.length > 0 ? articles.map(renderArticle) : null}
         </div>
     );
 });
