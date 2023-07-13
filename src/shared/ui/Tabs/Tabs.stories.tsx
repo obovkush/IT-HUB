d1bf736f004a6d59297f6f659c6aa10299e00354
Олег Бовкуш <obovkush@gmail.com>
2023-07-13 11:54:35 +1000

    Добавлен компонент Tabs

---
+++ src/shared/ui/Tabs/Tabs.stories.tsx
@@ -0,0 +1,32 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {Tabs} from './Tabs';
+
+const meta: Meta<typeof Tabs> = {
+    title: 'shared/Tabs',
+    component: Tabs,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tabs>;
+
+export const Default: Story = {};
+Default.args = {
+    tabs: [
+        {
+            value: 'tab 1',
+            content: 'tab 1',
+        },
+        {
+            value: 'tab 2',
+            content: 'tab 2',
+        },
+        {
+            value: 'tab 3',
+            content: 'tab 3',
+        },
+    ],
+    value: 'tab 2',
+    onTabClick: action('onTabClick'),
+};
