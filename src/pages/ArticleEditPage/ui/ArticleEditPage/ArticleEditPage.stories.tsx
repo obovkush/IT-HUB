f9ddaa696eb1eb8994ba7eddd4a8bfcef78827ab
Олег Бовкуш <obovkush@gmail.com>
2023-07-14 14:00:07 +1000

    Добавлена страница ArticleEditPage

---
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.stories.tsx
@@ -0,0 +1,17 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import ArticleEditPage from './ArticleEditPage';
+
+const meta: Meta<typeof ArticleEditPage> = {
+    title: 'pages/ArticleEditPage',
+    component: ArticleEditPage,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ArticleEditPage>;
+
+export const Default: Story = {
+    render: () => <ArticleEditPage />,
+};
+Default.args = {};
