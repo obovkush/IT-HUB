2ecb9e0986d50c5044bfb1ccab0353239568c508
Олег Бовкуш <obovkush@gmail.com>
2023-07-14 13:58:58 +1000

    Добавлен getCanEditArticle, экпортированы методы из API

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -7,3 +7,7 @@ export {ArticleList} from './ui/ArticleList/ArticleList';
 export {ArticleViewSelector} from './ui/ArticleViewSelector/ArticleViewSelector';
 export {ArticleSortSelector} from './ui/ArticleSortSelector/ArticleSortSelector';
 export {ArticleTypeTabs} from './ui/ArticleTypeTabs/ArticleTypeTabs';
+
+export {getArticleDetailsData} from './model/selectors/articleDetails';
+export {getArticleDetailsIsLoading} from './model/selectors/articleDetails';
+export {getArticleDetailsError} from './model/selectors/articleDetails';
