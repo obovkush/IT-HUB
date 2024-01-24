597e230024ebcc81eca3faf44129a91c07d103aa
Олег Бовкуш <o.bovkush@bftcom.com>
2024-01-24 17:45:32 +1000

    Исправлены ошибки в Dropdown

--- src/shared/ui/Dropdown/Dropdown.tsx
+++ src/shared/ui/Dropdown/Dropdown.tsx
@@ -28,7 +28,7 @@ const mapDirectionClass: Record<DropdownDirection, string> = {
 
 export function Dropdown(props: DropdownProps) {
     const {
-        className, trigger, items, direction = 'bottom right',
+        className = '', trigger, items, direction = 'bottom right',
     } = props;
 
     const menuClasses = [mapDirectionClass[direction]];
@@ -39,13 +39,13 @@ export function Dropdown(props: DropdownProps) {
                 {trigger}
             </Menu.Button>
             <Menu.Items className={classNames(cls.menu, {}, menuClasses)}>
-                {items.map((item) => {
+                {items.map((item, index) => {
                     const content = ({ active }: {active: boolean}) => (
                         <button
                             type="button"
                             disabled={item.disabled}
                             onClick={item.onClick}
-                            className={classNames(cls.item, { [cls.active]: active })}
+                            className={classNames(cls.item, { [cls.active]: active }, [])}
                         >
                             {item.content}
                         </button>
@@ -53,14 +53,14 @@ export function Dropdown(props: DropdownProps) {
 
                     if (item.href) {
                         return (
-                            <Menu.Item as={AppLink} to={item.href} disabled={item.disabled}>
+                            <Menu.Item as={AppLink} to={item.href} disabled={item.disabled} key={index}>
                                 {content}
                             </Menu.Item>
                         );
                     }
 
                     return (
-                        <Menu.Item as={Fragment} disabled={item.disabled}>
+                        <Menu.Item as={Fragment} disabled={item.disabled} key={index}>
                             {content}
                         </Menu.Item>
                     );
