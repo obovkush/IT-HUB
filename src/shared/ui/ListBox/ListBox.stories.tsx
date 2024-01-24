a172cb5ced3399b60259452fb381ad4132fce772
Олег Бовкуш <o.bovkush@bftcom.com>
2024-01-24 17:39:02 +1000

    Добавлены варианты расположения ListBox

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -4,6 +4,9 @@ import { ListBox } from './ListBox';
 const meta: Meta<typeof ListBox> = {
     title: 'shared/ListBox',
     component: ListBox,
+    decorators: [
+        (Story) => <div style={{ padding: 100 }}><Story /></div>,
+    ],
 };
 
 export default meta;
@@ -11,5 +14,55 @@ export default meta;
 type Story = StoryObj<typeof ListBox>;
 
 export const Default: Story = {
-    args: {},
+    args: {
+        value: 'Default',
+        items: [
+            { content: 'Menu 1', value: 'Menu1' },
+            { content: 'Menu 2', value: 'Menu2' },
+        ],
+    },
+};
+
+export const TopLeft: Story = {
+    args: {
+        direction: 'top left',
+        value: 'TopLeft',
+        items: [
+            { content: 'Menu 1', value: 'Menu1' },
+            { content: 'Menu 2', value: 'Menu2' },
+        ],
+    },
+};
+
+export const TopRight: Story = {
+    args: {
+        direction: 'top right',
+        value: 'TopRight',
+        items: [
+            { content: 'Menu 1', value: 'Menu1' },
+            { content: 'Menu 2', value: 'Menu2' },
+        ],
+    },
+};
+
+export const BottomLeft: Story = {
+    args: {
+        direction: 'bottom left',
+        value: 'BottomLeft',
+        items: [
+            { content: 'Menu 1', value: 'Menu1' },
+            { content: 'Menu 2', value: 'Menu2' },
+        ],
+    },
+};
+
+export const BottomRight: Story = {
+    args: {
+        direction: 'bottom right',
+        value: 'BottomRight',
+        items: [
+            { content: 'Menu 1', value: 'Menu1' },
+            { content: 'Menu 2', value: 'Menu2' },
+        ],
+    },
 };
