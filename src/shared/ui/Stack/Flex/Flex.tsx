233a63186221ddf8be295114e7e9f10dda064112
Олег Бовкуш <o.bovkush@bftcom.com>
2024-01-24 14:56:44 +1000

    Для Flex интерфейс расширен DivProps

--- src/shared/ui/Stack/Flex/Flex.tsx
+++ src/shared/ui/Stack/Flex/Flex.tsx
@@ -1,5 +1,5 @@
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
-import { ReactNode } from 'react';
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 import cls from './Flex.module.scss';
 
 export type FlexJustify = 'start' | 'center' | 'end' | 'between';
@@ -32,7 +32,9 @@ const gapClasses: Record<FlexGap, string> = {
     32: cls.gap32,
 };
 
-export interface FlexProps {
+type DivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+export interface FlexProps extends DivProps {
     className?: string;
     children: ReactNode;
     justify?: FlexJustify;
@@ -44,13 +46,13 @@ export interface FlexProps {
 
 export const Flex = (props: FlexProps) => {
     const {
-        className,
+        className = '',
         children,
         justify = 'start',
         align = 'center',
         direction = 'row',
-        gap,
-        max,
+        gap = 4,
+        max = false,
     } = props;
 
     const classes = [
