40027c1089090b9100fc3bdb22a4cba49f05f8fd
Олег Бовкуш <o.bovkush@bftcom.com>
2024-01-31 22:05:52 +1000

    Генератор фичей сущностей nodejs

--- src/shared/lib/url/addQueryParams/addQueryParams.ts
+++ src/shared/lib/url/addQueryParams/addQueryParams.ts
@@ -1,5 +1,5 @@
 export function getQueryParams(params: OptionalRecord<string, string>) {
-    const searchParams = new URLSearchParams(window.location.search);
+    const searchParams = new URLSearchParams();
     Object.entries(params).forEach(([name, value]) => {
         if (value !== undefined) {
             searchParams.set(name, value);
