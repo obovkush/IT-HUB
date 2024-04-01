7b428bf2004d824cdf7b1f1ba8aacebcdeb03659
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-02 09:59:10 +1000

    Доработан index app

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from 'app/App';
 import {BrowserRouter} from 'react-router-dom';
 import {ThemeProvider} from 'app/providers/ThemeProvider';
@@ -7,7 +7,16 @@ import 'app/styles/index.scss';
 import './shared/config/i18n/i18n';
 import {ErrorBoundary} from 'app/providers/ErrorBoundary';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+const container = document.getElementById('root');
+
+if (!container) {
+    throw new Error('Контейнер root не найден. НЕ удалось вмонтировать реакт приложение');
+}
+
+const root = createRoot(container);
+
+root.render(
     <BrowserRouter>
         <StoreProvider>
             <ErrorBoundary>
@@ -16,5 +25,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
                 </ThemeProvider>
             </ErrorBoundary>
         </StoreProvider>
-    </BrowserRouter>
+    </BrowserRouter>,
 );
