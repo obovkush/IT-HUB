24d711bd12751ff3cce001988f9cf01eea3176b4
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-02 16:42:50 +1000

    Установлена и настроена библиотека circular-dependency-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import {BuildOptions} from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 
 export function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
     const {paths, isDev, apiUrl, project} = options;
@@ -26,6 +27,10 @@ export function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
             patterns: [
                 { from: paths.locales, to: paths.buildLocales },
             ],
+        }),
+        new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            failOnError: true,
         }),
     ];
 
