c113e1ce4f099152b10c7fc4d4b10a231db0d6b6
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-02 11:46:37 +1000

    storybook-addon-mock добавлен в конфиг addons storybook

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
         "storybook-addon-react-router-v6",
+        'storybook-addon-mock',
     ],
     framework: {
         name: '@storybook/react-webpack5',
