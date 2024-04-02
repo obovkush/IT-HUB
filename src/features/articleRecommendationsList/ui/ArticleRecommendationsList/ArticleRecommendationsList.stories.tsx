3aecbc82f50916f99801a15badf640ff173cc56d
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-02 11:47:37 +1000

    Сторис ArticleRecommendationsList с моком

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories.tsx
@@ -1,6 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import {StoreDecorator} from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import {ArticleRecommendationsList} from './ArticleRecommendationsList';
+import {Article} from 'entities/Article';
 
 
 const meta: Meta<typeof ArticleRecommendationsList> = {
@@ -12,7 +13,33 @@ export default meta;
 
 type Story = StoryObj<typeof ArticleRecommendationsList>;
 
+const article: Article = {
+    id: '1',
+    img: '',
+    createdAt: '',
+    views: 123,
+    user: { id: '1', username: 'admin' },
+    blocks: [],
+    type: [],
+    title: 'TEST',
+    subtitle: 'subtest',
+};
+
 export const Default: Story = {
     render: () => <ArticleRecommendationsList />,
 };
 Default.decorators = [StoreDecorator({})];
+Default.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/articles?_limit=3`,
+            method: 'GET',
+            status: 200,
+            response: [
+                { ...article, id: '1' },
+                { ...article, id: '2' },
+                { ...article, id: '3' },
+            ],
+        },
+    ],
+};
