bf85d99c43721d1777f1b25762748c31fa77af4f
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-02 11:16:03 +1000

    Добавлен SuspenseDecorator в Preview storybook

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -2,7 +2,9 @@ import {Preview} from '@storybook/react';
 import {StyleDecorator} from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import {ThemeDecorator} from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import {RouterDecorator} from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import {SuspenseDecorator} from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
 import {Theme} from '../../src/app/providers/ThemeProvider';
+
 
 const preview: Preview = {
     parameters: {
@@ -14,7 +16,7 @@ const preview: Preview = {
             },
         },
     },
-    decorators: [StyleDecorator, ThemeDecorator(Theme.LIGHT), RouterDecorator],
+    decorators: [StyleDecorator, ThemeDecorator(Theme.LIGHT), RouterDecorator, SuspenseDecorator],
 
     globalTypes: {
         theme: {
