91bf69a0eaeb31fa7ab3ef51ae556d8b83beca53
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-03 11:43:31 +1000

    Добавлен компонент NotificationButton

---
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
@@ -0,0 +1,15 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {NotificationButton} from './NotificationButton';
+
+const meta: Meta<typeof NotificationButton> = {
+    title: 'features/NotificationButton',
+    component: NotificationButton,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof NotificationButton>;
+
+export const Default: Story = {
+    render: () => <NotificationButton />,
+};
