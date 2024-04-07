657496727f3cbe017d6c5a2c696540d6f86301ba
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-07 21:27:30 +1000

    Добавлен мод fullWidth в Button

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -25,6 +25,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	size?: ButtonSize;
 	disabled?: boolean;
 	children?: ReactNode;
+    fullWidth?: boolean;
 }
 
 export const Button: FC<ButtonProps> = memo((props) => {
@@ -35,6 +36,7 @@ export const Button: FC<ButtonProps> = memo((props) => {
         square,
         size = ButtonSize.M,
         disabled,
+        fullWidth,
         ...otherProps
     } = props;
 
@@ -43,6 +45,7 @@ export const Button: FC<ButtonProps> = memo((props) => {
         [cls.square]: square,
         [cls[size]]: true,
         [cls.disabled]: disabled,
+        [cls.fullWidth]: fullWidth,
     };
 
     return (
