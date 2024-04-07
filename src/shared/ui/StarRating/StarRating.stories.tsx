59db6186148a6242682c0dd1595c105ab2f29c6a
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-07 21:28:05 +1000

    Добавлен компонент StarRating

---
+++ src/shared/ui/StarRating/StarRating.stories.tsx
@@ -0,0 +1,16 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {StarRating} from './StarRating';
+
+
+const meta: Meta<typeof StarRating> = {
+    title: 'entities/StarRating',
+    component: StarRating,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof StarRating>;
+
+export const Default: Story = {
+    render: () => <StarRating />,
+};
