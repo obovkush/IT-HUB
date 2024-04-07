a7df00a4496531de553ff25632d3a4aa10075c59
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-07 21:27:03 +1000

    Доработаны пропсы Icon

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -2,14 +2,14 @@ import {classNames} from '@/shared/lib/classNames/classNames';
 import React, {memo} from 'react';
 import cls from './Icon.module.scss';
 
-interface IconProps {
-	className?: string;
-	Svg: React.FC<React.SVGProps<SVGSVGElement>>;
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+    className?: string;
+    Svg: React.VFC<React.SVGProps<SVGSVGElement>>;
     inverted?: boolean;
 }
 
 export const Icon = memo((props: IconProps) => {
-    const {className = '', Svg, inverted} = props;
+    const {className = '', Svg, inverted, ...otherProps} = props;
 
-    return <Svg className={classNames(inverted ? cls.inverted : cls.Icon, {}, [className])} />;
+    return <Svg className={classNames(inverted ? cls.inverted : cls.Icon, {}, [className])} {...otherProps} />;
 });
