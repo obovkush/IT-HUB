363cbac2384580f03c282e6a99523fbbae16978c
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-08 22:25:53 +1000

    TODO импорты в StoreDecorator

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,10 +1,15 @@
 import {StoryFn} from '@storybook/react';
 import {StateSchema, StoreProvider} from '@/app/providers/StoreProvider';
+// eslint-disable-next-line import-module/public-api-imports
 import {loginReducer} from '@/features/AuthByUsername/model/slice/loginSlice';
+// eslint-disable-next-line import-module/public-api-imports
 import {profileReducer} from '@/features/editableProfileCard/model/slice/profileSlice';
+// eslint-disable-next-line import-module/public-api-imports
 import {articleDetailsReducer} from '@/entities/Article/model/slice/articleDetailsSlice';
 import {ReducersList} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+// eslint-disable-next-line import-module/public-api-imports
 import {addCommentFormReducer} from '@/features/addCommentForm/model/slices/addCommentFormSlice';
+// eslint-disable-next-line import-module/public-api-imports
 import {articleDetailsPageReducer} from '@/pages/ArticleDetailsPage/model/slices';
 
 const defaultAsyncReducers: ReducersList = {
