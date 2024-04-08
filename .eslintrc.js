d896633116b7b7c121576476e408590aff34e8bd
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-08 19:53:47 +1000

    Установлен и настроен "eslint-plugin-import-module": "^0.0.3"

--- .eslintrc.js
+++ .eslintrc.js
@@ -64,7 +64,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'import-module/path-checker': 'error',
+        'import-module/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
