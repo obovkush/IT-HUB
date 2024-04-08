3c1096b5f002fcf5107321b013169c86a4d49125
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-08 22:55:10 +1000

    Исправлены импорты редьюсеров в StoreDecorator, созданы отдельные файлы экспортов для тестов

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,16 +1,11 @@
 import {StoryFn} from '@storybook/react';
 import {StateSchema, StoreProvider} from '@/app/providers/StoreProvider';
-// eslint-disable-next-line import-module/public-api-imports
-import {loginReducer} from '@/features/AuthByUsername/model/slice/loginSlice';
-// eslint-disable-next-line import-module/public-api-imports
-import {profileReducer} from '@/features/editableProfileCard/model/slice/profileSlice';
-// eslint-disable-next-line import-module/public-api-imports
-import {articleDetailsReducer} from '@/entities/Article/model/slice/articleDetailsSlice';
+import {loginReducer} from '@/features/AuthByUsername/testing';
+import {profileReducer} from '@/features/editableProfileCard/testing';
+import {articleDetailsReducer} from '@/entities/Article/testing';
+import {addCommentFormReducer} from '@/features/addCommentForm/testing';
+import {articleDetailsPageReducer} from '@/pages/ArticleDetailsPage/testing';
 import {ReducersList} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-// eslint-disable-next-line import-module/public-api-imports
-import {addCommentFormReducer} from '@/features/addCommentForm/model/slices/addCommentFormSlice';
-// eslint-disable-next-line import-module/public-api-imports
-import {articleDetailsPageReducer} from '@/pages/ArticleDetailsPage/model/slices';
 
 const defaultAsyncReducers: ReducersList = {
     loginForm: loginReducer,
