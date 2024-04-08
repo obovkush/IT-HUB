dc6ef2f4f2b6bab4c8300c100c26bdf848b49b1c
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-08 11:54:53 +1000

    Перенесены сторис Notification

--- src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -1,9 +1,11 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import {NotificationList} from './NotificationList';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 const meta: Meta<typeof NotificationList> = {
-    title: 'entity/NotificationList',
+    title: 'entities/Notification/NotificationList',
     component: NotificationList,
+    decorators: [StoreDecorator({})],
 };
 
 export default meta;
