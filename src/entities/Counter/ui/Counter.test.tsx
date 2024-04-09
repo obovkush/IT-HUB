9cd92f092796a2bf818b0f2ed8bb07550198ea19
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-09 19:49:17 +1000

    Фикс теста Counter

--- src/entities/Counter/ui/Counter.test.tsx
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -1,5 +1,5 @@
-import {userEvent} from '@storybook/testing-library';
 import {screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import {componentRender} from '@/shared/lib/tests/componentRender/componentRender';
 
@@ -14,19 +14,19 @@ describe('Counter', () => {
         expect(screen.getByTestId('value-title')).toHaveTextContent('10');
     });
 
-    test('increment', () => {
+    test('increment', async () => {
         componentRender(<Counter />, {
             initialState: {counter: {value: 10}},
         });
-        userEvent.click(screen.getByTestId('increment-btn'));
+        await userEvent.click(screen.getByTestId('increment-btn'));
         expect(screen.getByTestId('value-title')).toHaveTextContent('11');
     });
 
-    test('decrement', () => {
+    test('decrement', async () => {
         componentRender(<Counter />, {
             initialState: {counter: {value: 10}},
         });
-        userEvent.click(screen.getByTestId('decrement-btn'));
+        await userEvent.click(screen.getByTestId('decrement-btn'));
         expect(screen.getByTestId('value-title')).toHaveTextContent('9');
     });
 });
