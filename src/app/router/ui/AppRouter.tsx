3e23235fa6e254b1d81cdf8394e0fd1c40729f54
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-09 10:19:39 +1000

    Исправление импорта AppRoutesProps в AppRouter

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -1,30 +1,19 @@
 import {Suspense, memo, useCallback} from 'react';
 import {Route, Routes} from 'react-router-dom';
-import {AppRoutesProps, router} from './config/routerConfig';
+import {router} from './config/routerConfig';
 import {PageLoader} from '@/widjets/PageLoader';
 import {RequireAuth} from './RequireAuth';
+import {AppRoutesProps} from '@/shared/types/router';
 
 const AppRouter = () => {
     const renderWithWrapper = useCallback((route: AppRoutesProps) => {
-        const element = (
-            <Suspense fallback={<PageLoader />}>
-                {route.element}
-            </Suspense>
-        );
+        const element = <Suspense fallback={<PageLoader />}>{route.element}</Suspense>;
         return (
-            <Route
-                key={route.path}
-                path={route.path}
-                element={route.authOnly ? <RequireAuth roles={route.roles}>{element}</RequireAuth> : element}
-            />
+            <Route key={route.path} path={route.path} element={route.authOnly ? <RequireAuth roles={route.roles}>{element}</RequireAuth> : element} />
         );
     }, []);
 
-    return (
-        <Routes>
-            {Object.values(router).map(renderWithWrapper)}
-        </Routes>
-    );
+    return <Routes>{Object.values(router).map(renderWithWrapper)}</Routes>;
 };
 
 export default memo(AppRouter);
