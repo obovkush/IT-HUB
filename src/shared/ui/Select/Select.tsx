e588fecc78a50283f438a3aae9db8e7677e61555
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-09 22:15:57 +1000

    Фикс импортов в селекте

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,7 +1,7 @@
 import {ChangeEvent, useMemo} from 'react';
 
-import {typedMemo} from '@/shared/lib/typedMemo/typedMemo';
 import {classNames, Mods} from '@/shared/lib/classNames/classNames';
+import {typedMemo} from '@/shared/lib/typedMemo/typedMemo';
 
 import cls from './Select.module.scss';
 
