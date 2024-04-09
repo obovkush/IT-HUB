60c6ea4ebb45f2dc1e693618fc7f065b7880b8ac
Олег Бовкуш <o.bovkush@bftcom.com>
2024-04-09 15:43:03 +1000

    Фикс порядка импортов ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,11 @@ import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 
 import App from '@/app/App';
+import {ErrorBoundary} from '@/app/providers/ErrorBoundary';
 import {StoreProvider} from '@/app/providers/StoreProvider';
 import {ThemeProvider} from '@/app/providers/ThemeProvider';
 import '@/app/styles/index.scss';
 import './shared/config/i18n/i18n';
-import {ErrorBoundary} from '@/app/providers/ErrorBoundary';
-
 
 const container = document.getElementById('root');
 
