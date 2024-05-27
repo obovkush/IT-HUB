1b01ba0b9c982a15237975c3fa0307c96f5675a3
Олег Бовкуш <o.bovkush@bftcom.com>
2024-05-27 21:57:15 +1000

    Исправлены ошибки установки зависимостей

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
@@ -1,11 +1,12 @@
 import {Suspense, memo, useCallback} from 'react';
 
 import {useTranslation} from 'react-i18next';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 
 import {CommentList} from '@/entities/Comment';
 import {AddCommentForm} from '@/features/addCommentForm';
 import {classNames} from '@/shared/lib/classNames/classNames';
+import {useAppDispatch} from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import {useInitialEffect} from '@/shared/lib/hooks/useInitialEffect/useInitialEffect';
 import {VStack} from '@/shared/ui/Stack';
 import {Text, TextSize} from '@/shared/ui/Text';
@@ -25,7 +26,7 @@ export const ArticleDetailsComments = memo((props: ArticleDetailsCommentsProps) 
     const {t} = useTranslation();
     const comments = useSelector(getArticleComments.selectAll);
     const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
 
     const onSendComment = useCallback(
         (text: string) => {
