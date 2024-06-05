d6d0c5f9c97092388841e37e36d136e8c5c54e52
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-05 15:34:17 +1000

    В карточке статьи добавлен переключатель фичей в зависимости от пользователя

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -8,6 +8,8 @@ import {ArticleRating} from '@/features/articleRating';
 import {ArticleRecommendationsList} from '@/features/articleRecommendationsList';
 import {classNames} from '@/shared/lib/classNames/classNames';
 import {DynamicModuleLoader, ReducersList} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import {toggleFeatures} from '@/shared/lib/features';
+import {Card} from '@/shared/ui/Card';
 import {VStack} from '@/shared/ui/Stack';
 import {Page} from '@/widgets/Page';
 
@@ -33,13 +35,19 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
         return <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>{t('Статья не найдена')}</Page>;
     }
 
+    const articleRatingCard = toggleFeatures({
+        name: 'isArticleRatingEnabled',
+        on: () => <ArticleRating articleId={id} />,
+        off: () => <Card>{t('Оценка статей скоро появится!')}</Card>,
+    });
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
                 <VStack gap='16' max>
                     <ArticleDetailsPageHeader />
                     <ArticleDetails id={id} />
-                    <ArticleRating articleId={id} />
+                    {articleRatingCard}
                     <ArticleRecommendationsList />
                     <ArticleDetailsComments id={id} />
                 </VStack>
