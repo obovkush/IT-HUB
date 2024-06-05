88116b12cb1ba6d595fdd95c62924a9d298c22d1
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-05 15:33:34 +1000

    Добавлена запись в стейт флагов фичей для пользователя

--- src/entities/User/model/types/user.ts
+++ src/entities/User/model/types/user.ts
@@ -1,13 +1,16 @@
+import {FeatureFlags} from '@/shared/types/featureFlags';
+
 import {UserRole} from '../consts/userConsts';
 
 export interface User {
-	id: string;
-	username: string;
-	avatar?: string;
-	roles?: UserRole[];
+    id: string;
+    username: string;
+    avatar?: string;
+    roles?: UserRole[];
+    features?: FeatureFlags;
 }
 
 export interface UserSchema {
-	authData?: User;
-	_inited: boolean;
+    authData?: User;
+    _inited: boolean;
 }
