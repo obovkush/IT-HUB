9e8fd0947fb1ae5d298b5f70607848c969883b27
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-11 12:01:09 +1000

    Фикс путей в Modal

--- src/shared/ui/deprecated/Modal/Modal.tsx
+++ src/shared/ui/deprecated/Modal/Modal.tsx
@@ -5,15 +5,15 @@ import {useModal} from '@/shared/lib/hooks/useModal/useModal';
 import {useTheme} from '@/shared/lib/hooks/useTheme/useTheme';
 
 import cls from './Modal.module.scss';
-import {Overlay} from '../Overlay/Overlay';
-import {Portal} from '../Portal/Portal';
+import {Overlay} from '../../redesigned/Overlay/Overlay';
+import {Portal} from '../../redesigned/Portal/Portal';
 
 interface ModalProps {
-	className?: string;
-	children?: ReactNode;
-	isOpen?: boolean;
-	onClose?: () => void;
-	lazy?: boolean;
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+    lazy?: boolean;
 }
 /**
  * Устарел, новые компоненты в каталоге redesigned
@@ -25,11 +25,7 @@ export const Modal = (props: ModalProps) => {
     const {className = '', children, isOpen, onClose, lazy} = props;
     const {theme} = useTheme();
 
-    const {
-        close,
-        isClosing,
-        isMounted,
-    } = useModal({
+    const {close, isClosing, isMounted} = useModal({
         animationDelay: ANIMATION_DELAY,
         onClose,
         isOpen,
