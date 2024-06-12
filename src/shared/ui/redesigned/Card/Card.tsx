d954cdd285d6185e3ec736b57bc652a6cdee5b5c
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-12 17:15:54 +1000

    Добавлены варианты закругления для карточки

--- src/shared/ui/redesigned/Card/Card.tsx
+++ src/shared/ui/redesigned/Card/Card.tsx
@@ -6,6 +6,7 @@ import cls from './Card.module.scss';
 
 export type CardVariant = 'normal' | 'outlined' | 'light';
 export type CardPadding = '0' | '8' | '16' | '24';
+export type CardBorder = 'normal' | 'round';
 
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
@@ -13,6 +14,7 @@ interface CardProps extends HTMLAttributes<HTMLDivElement> {
     variant?: CardVariant;
     max?: boolean;
     padding?: CardPadding;
+    border?: CardBorder;
 }
 
 const mapPaddingToClass: Record<CardPadding, string> = {
@@ -22,13 +24,19 @@ const mapPaddingToClass: Record<CardPadding, string> = {
     '24': 'gap_24',
 };
 
+const mapBorderToClass: Record<CardBorder, string> = {
+    normal: 'normal_border',
+    round: 'round_border',
+};
+
 export const Card = memo((props: CardProps) => {
-    const {className = '', children, variant = 'normal', max, padding = '8', ...otherProps} = props;
+    const {className = '', children, variant = 'normal', max, padding = '8', border = 'normal', ...otherProps} = props;
 
     const paddingClass = mapPaddingToClass[padding];
+    const borderClass = mapBorderToClass[border];
 
     return (
-        <div className={classNames(cls.Card, {[cls.max]: max}, [className, cls[variant], cls[paddingClass]])} {...otherProps}>
+        <div className={classNames(cls.Card, {[cls.max]: max}, [className, cls[variant], cls[paddingClass], cls[borderClass]])} {...otherProps}>
             {children}
         </div>
     );
