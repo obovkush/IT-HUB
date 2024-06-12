ef6741e2841e814c82c02d1f2153844bc0b6b7df
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-12 22:42:15 +1000

    В classNames заданы значения агументов mods, additional по умолчанию

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,8 +2,8 @@ export type Mods = Record<string, boolean | string | undefined>;
 
 export function classNames(
     cls: string,
-    mods: Mods,
-    additional: string[]
+    mods: Mods = {},
+    additional: string[] = []
 ): string {
     return [
         cls,
