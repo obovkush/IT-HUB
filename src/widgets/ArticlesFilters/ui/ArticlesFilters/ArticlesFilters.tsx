a52201c642bbce51dc36ae3a220bf6990029946a
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-12 21:38:07 +1000

    Редизайн поля поиска в ArticlesFilters

--- src/widgets/ArticlesFilters/ui/ArticlesFilters/ArticlesFilters.tsx
+++ src/widgets/ArticlesFilters/ui/ArticlesFilters/ArticlesFilters.tsx
@@ -5,9 +5,11 @@ import {useTranslation} from 'react-i18next';
 import {ArticleSortField, ArticleType} from '@/entities/Article';
 import {ArticleSortSelector} from '@/features/ArticleSortSelector';
 import {ArticleTypeTabs} from '@/features/ArticleTypeTabs';
+import SearchIcon from '@/shared/assets/icons/search.svg';
 import {classNames} from '@/shared/lib/classNames/classNames';
 import {SortOrder} from '@/shared/types/sort';
 import {Card} from '@/shared/ui/redesigned/Card';
+import {Icon} from '@/shared/ui/redesigned/Icon';
 import {Input} from '@/shared/ui/redesigned/Input';
 import {VStack} from '@/shared/ui/redesigned/Stack';
 
@@ -32,7 +34,7 @@ export const ArticlesFilters = memo((props: ArticlesFiltersProps) => {
     return (
         <Card className={classNames(cls.ArticlesFilters, {}, [className])} padding='24'>
             <VStack gap='32'>
-                <Input onChange={onChangeSearch} value={search} placeholder={t('Поиск')} />
+                <Input onChange={onChangeSearch} value={search} placeholder={t('Поиск')} addonLeft={<Icon Svg={SearchIcon} />} />
                 <ArticleTypeTabs value={type} onChangeType={onChangeType} className={cls.tabs} />
                 <ArticleSortSelector order={order} sort={sort} onChangeOrder={onChangeOrder} onChangeSort={onChangeSort} />
             </VStack>
