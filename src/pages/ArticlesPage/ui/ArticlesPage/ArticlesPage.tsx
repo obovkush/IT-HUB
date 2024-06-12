5698ff8ef78210494799db99ed4095c5f6c6c5fa
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-12 17:31:15 +1000

    Редизайн ArticlesPage

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,11 +1,12 @@
 import {memo, useCallback} from 'react';
 
-import {useTranslation} from 'react-i18next';
 import {useSearchParams} from 'react-router-dom';
 
 import {ArticlePageGreeting} from '@/features/articlePageGreeting';
+import {StickyContentLayout} from '@/shared/layouts/StickyContentLayout';
 import {classNames} from '@/shared/lib/classNames/classNames';
 import {DynamicModuleLoader, ReducersList} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import {ToggleFeatures} from '@/shared/lib/features';
 import {useAppDispatch} from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import {useInitialEffect} from '@/shared/lib/hooks/useInitialEffect/useInitialEffect';
 import {Page} from '@/widgets/Page';
@@ -16,6 +17,8 @@ import {initArticlesPage} from '../../model/services/initArticlesPage/initArticl
 import {articlesPageReducer} from '../../model/slices/articlesPageSlice';
 import {ArticleInfiniteList} from '../ArticleInfiniteList/ArticleInfiniteList';
 import {ArticlesPageFilters} from '../ArticlesPageFilters/ArticlesPageFilters';
+import {FiltersContainer} from '../FiltersContainer/FiltersContainer';
+import {ViewSelectorContainer} from '../ViewSelectorContainer/ViewSelectorContainer';
 
 interface ArticlesPageProps {
     className?: string;
@@ -27,25 +30,50 @@ const reducers: ReducersList = {
 
 const ArticlesPage = (props: ArticlesPageProps) => {
     const {className = ''} = props;
-    const {} = useTranslation();
+
     const dispatch = useAppDispatch();
     const [searchParams] = useSearchParams();
 
     const onLoadNextPart = useCallback(() => {
-        if (__PROJECT__ !== 'storybook') dispatch(fetchNextArticlesPage());
+        dispatch(fetchNextArticlesPage());
     }, [dispatch]);
 
     useInitialEffect(() => {
         dispatch(initArticlesPage(searchParams));
     });
 
+    const content = (
+        <ToggleFeatures
+            feature='isAppRedesigned'
+            on={
+                <StickyContentLayout
+                    left={<ViewSelectorContainer />}
+                    right={<FiltersContainer />}
+                    content={
+                        <Page
+                            data-testid='ArticlesPage'
+                            onScrollEnd={onLoadNextPart}
+                            className={classNames(cls.ArticlesPageRedesigned, {}, [className])}
+                        >
+                            <ArticleInfiniteList className={cls.list} />
+                            <ArticlePageGreeting />
+                        </Page>
+                    }
+                />
+            }
+            off={
+                <Page data-testid='ArticlesPage' onScrollEnd={onLoadNextPart} className={classNames(cls.ArticlesPage, {}, [className])}>
+                    <ArticlesPageFilters />
+                    <ArticleInfiniteList className={cls.list} />
+                    <ArticlePageGreeting />
+                </Page>
+            }
+        />
+    );
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
-            <Page data-testid='ArticlesPage' onScrollEnd={onLoadNextPart} className={classNames(cls.ArticlesPage, {}, [className])}>
-                <ArticlesPageFilters />
-                <ArticleInfiniteList className={cls.list} />
-                <ArticlePageGreeting />
-            </Page>
+            {content}
         </DynamicModuleLoader>
     );
 };
