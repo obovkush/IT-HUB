577044798aa5874ddca841773c4712ad863d07f7
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-12 17:28:08 +1000

    Добавлен ArticlesFilters

---
+++ src/widgets/ArticlesFilters/ui/ArticlesFilters/ArticlesFilters.tsx
@@ -0,0 +1,41 @@
+import {memo} from 'react';
+
+import {useTranslation} from 'react-i18next';
+
+import {ArticleSortField, ArticleType} from '@/entities/Article';
+import {ArticleSortSelector} from '@/features/ArticleSortSelector';
+import {ArticleTypeTabs} from '@/features/ArticleTypeTabs';
+import {classNames} from '@/shared/lib/classNames/classNames';
+import {SortOrder} from '@/shared/types/sort';
+import {Card} from '@/shared/ui/redesigned/Card';
+import {Input} from '@/shared/ui/redesigned/Input';
+import {VStack} from '@/shared/ui/redesigned/Stack';
+
+import cls from './ArticlesFilters.module.scss';
+
+interface ArticlesFiltersProps {
+    className?: string;
+    sort: ArticleSortField;
+    order: SortOrder;
+    type: ArticleType;
+    search: string;
+    onChangeSearch: (value: string) => void;
+    onChangeOrder: (newOrder: SortOrder) => void;
+    onChangeSort: (newSort: ArticleSortField) => void;
+    onChangeType: (type: ArticleType) => void;
+}
+
+export const ArticlesFilters = memo((props: ArticlesFiltersProps) => {
+    const {className = '', onChangeType, onChangeSearch, search, onChangeSort, sort, onChangeOrder, order, type} = props;
+    const {t} = useTranslation();
+
+    return (
+        <Card className={classNames(cls.ArticlesFilters, {}, [className])} padding='24'>
+            <VStack gap='32'>
+                <Input onChange={onChangeSearch} value={search} placeholder={t('Поиск')} />
+                <ArticleTypeTabs value={type} onChangeType={onChangeType} className={cls.tabs} />
+                <ArticleSortSelector order={order} sort={sort} onChangeOrder={onChangeOrder} onChangeSort={onChangeSort} />
+            </VStack>
+        </Card>
+    );
+});
