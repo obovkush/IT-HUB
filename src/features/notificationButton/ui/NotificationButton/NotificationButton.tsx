0415763b0f37e8b9d3eae9851b7f46f6782d86ab
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-18 11:00:47 +1000

    Обновление импортов Modal, Drawer

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -7,9 +7,9 @@ import {classNames} from '@/shared/lib/classNames/classNames';
 import {ToggleFeatures} from '@/shared/lib/features';
 import {useDevice} from '@/shared/lib/hooks/useDevice/useDevice';
 import {Button as ButtonDeprecated, ButtonTheme} from '@/shared/ui/deprecated/Button';
-import {Drawer} from '@/shared/ui/deprecated/Drawer';
 import {Icon as IconDeprecated} from '@/shared/ui/deprecated/Icon';
 import {Popover as PopoverDeprecated} from '@/shared/ui/deprecated/Popups';
+import {Drawer} from '@/shared/ui/redesigned/Drawer';
 import {Icon} from '@/shared/ui/redesigned/Icon';
 import {Popover} from '@/shared/ui/redesigned/Popups';
 
