e17c36d82c34b37355185a9e1efdd2310317eced
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-18 15:32:06 +1000

    Добавлены варианты цветов кнопки

--- src/shared/ui/redesigned/Button/Button.tsx
+++ src/shared/ui/redesigned/Button/Button.tsx
@@ -5,6 +5,7 @@ import {classNames, Mods} from '@/shared/lib/classNames/classNames';
 import cls from './Button.module.scss';
 
 export type ButtonVariant = 'clear' | 'outline' | 'filled';
+export type ButtonColor = 'normal' | 'success' | 'error';
 
 export type ButtonSize = 'm' | 'l' | 'xl';
 
@@ -14,6 +15,10 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
      * Тема кнопки. Отвечает за визуал (в рамке, без стилей, противоположный теме приложения цвет и тд)
      */
     variant?: ButtonVariant;
+    /**
+     * Цвет кнопки.
+     */
+    color?: ButtonColor;
     /**
      * Флаг, делающий кнопку квадратной
      */
@@ -40,7 +45,19 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button = memo((props: ButtonProps) => {
-    const {className = '', children, variant = 'outline', square, disabled, fullWidth, size = 'm', addonLeft, addonRight, ...otherProps} = props;
+    const {
+        className = '',
+        children,
+        variant = 'outline',
+        color = 'normal',
+        square,
+        disabled,
+        fullWidth,
+        size = 'm',
+        addonLeft,
+        addonRight,
+        ...otherProps
+    } = props;
 
     const mods: Mods = {
         [cls.square]: square,
@@ -50,7 +67,12 @@ export const Button = memo((props: ButtonProps) => {
     };
 
     return (
-        <button type='button' className={classNames(cls.Button, mods, [className, cls[variant], cls[size]])} disabled={disabled} {...otherProps}>
+        <button
+            type='button'
+            className={classNames(cls.Button, mods, [className, cls[variant], cls[size], cls[color]])}
+            disabled={disabled}
+            {...otherProps}
+        >
             {addonLeft && <div className={cls.addonLeft}>{addonLeft}</div>}
             {children}
             {addonRight && <div className={cls.addonRight}>{addonRight}</div>}
