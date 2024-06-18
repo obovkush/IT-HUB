e12e280bfa98d0d546ae8c41106857fcec2db3f7
Олег Бовкуш <o.bovkush@bftcom.com>
2024-06-18 15:32:24 +1000

    Добавлен вариант бордера карточки

--- src/shared/ui/redesigned/Card/Card.tsx
+++ src/shared/ui/redesigned/Card/Card.tsx
@@ -6,7 +6,7 @@ import cls from './Card.module.scss';
 
 export type CardVariant = 'normal' | 'outlined' | 'light';
 export type CardPadding = '0' | '8' | '16' | '24';
-export type CardBorder = 'normal' | 'round';
+export type CardBorder = 'normal' | 'round' | 'partial';
 
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
@@ -28,6 +28,7 @@ const mapPaddingToClass: Record<CardPadding, string> = {
 const mapBorderToClass: Record<CardBorder, string> = {
     normal: 'normal_border',
     round: 'round_border',
+    partial: 'partial_border',
 };
 
 export const Card = memo((props: CardProps) => {
